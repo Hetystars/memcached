44eb5e016563502a7d0963c79243741b50942cd2
hety <hetystars@gmail.com>
2019-12-13 09:39:01 +0800

    [N/A]hety:更新bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,9 +9,9 @@ if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
     $loader = require dirname(__DIR__, 3) . '/vendor/autoload.php';
 
     // need load testing psr4 config map
-    $componentDir  = dirname(__DIR__, 3);
+    $componentDir = dirname(__DIR__, 3);
     $componentJson = $componentDir . '/composer.json';
-    $composerData  = json_decode(file_get_contents($componentJson), true);
+    $composerData = json_decode(file_get_contents($componentJson), true);
     foreach ($composerData['autoload-dev']['psr-4'] as $prefix => $dir) {
         $loader->addPsr4($prefix, $componentDir . '/' . $dir);
     }
@@ -21,9 +21,9 @@ if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
     $loader = require dirname(__DIR__, 5) . '/autoload.php';
 
     // need load testing psr4 config map
-    $componentDir  = dirname(__DIR__, 3);
+    $componentDir = dirname(__DIR__, 3);
     $componentJson = $componentDir . '/composer.json';
-    $composerData  = json_decode(file_get_contents($componentJson), true);
+    $composerData = json_decode(file_get_contents($componentJson), true);
 
     foreach ($composerData['autoload-dev']['psr-4'] as $prefix => $dir) {
         $loader->addPsr4($prefix, $componentDir . '/' . $dir);
